1fc016e876f31c53999cc182ad261ea7eb133220
ROHIT RAJ YADAV <rohityadav21012001@gmail.com>
2025-09-08 03:13:26 +0530

    updated proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
   const target = process.env.REACT_APP_API_URL || 'http://localhost:8080';
 
   app.use(
-    ['/api', '/auth', '/users', '/games', '/health', '/socket.io'],
+    ['/api', '/auth', '/users', '/health', '/socket.io'],
     createProxyMiddleware({
       target,
       changeOrigin: true,
